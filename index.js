9612db5effcf405e27f8d82ae13541ba782eaf02
kevin-fl <kevinflamang@gmail.com>
2022-07-29 20:32:10 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//   Methode POST --> dans une requete HTTP --> avec fetch()
+
+// créer d un objet js pour l utilisateur
+
+const newUser = {
+    name: "kevin",
+    website: "https://www.google.com",
+
+};
+
+const promise = fetch("https://jsonplaceholder.typicode.com/users", {
+method: "POST",                                                                                     //-> methode POST envoi les infos au serveur , la const new user
+body: JSON.stringify(newUser),                                                                      //=> permet a l objet de la const new user d etre sous forme string 
+headers: {
+    "content-Type": "application/json",
+},
+
+});
+
+promise.then( async (response) => {                                                                  //-> promise quand valide renvoi .then async donc await 
+try{                                                                                                 //-> methode try avec async await if succes et catch if error 
+    console.log(response);                                                                           //-> le await response (le params de la fonction asynchrone) 
+const renvoiPostDansJson =   await response.json()                                                   //->response.json() pour avoir la reponse sous forme json 
+console.log(renvoiPostDansJson);                                                                     //-> nous permet de voir on a bien post la const new user dans ce website
+                                                                                                    //-> on peut le voir via la console 
+}catch (e) {
+console.log(e);
+}
+})
